48d5a86e1ec1b6114a8de0732cd43fdd3001eed8
Haloen <72333894+Haloen@users.noreply.github.com>
2022-08-09 20:10:03 +0800

    Trying gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  lintOnSave: false,
-  transpileDependencies: [
-    "vuetify",
-  ],
-  chainWebpack: (config) => {
-    config.module
-      .rule("ts")
-      .use("ts-loader")
-      .loader("ts-loader");
-  },
+    publicPath: process.env.NODE_ENV == "production" ? "/ISAD/" : "/",
+    lintOnSave: false,
+    transpileDependencies: [
+        "vuetify",
+    ],
+    chainWebpack: (config) => {
+        config.module
+        .rule("ts")
+        .use("ts-loader")
+        .loader("ts-loader");
+    },
 };
