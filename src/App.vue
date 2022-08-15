32c7595280f9bf106336a8111760bd14ffee15a8
Haloen <72333894+Haloen@users.noreply.github.com>
2022-08-15 09:05:23 +0800

    Animation Tutorial done :)

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
     <v-app class="grey lighten-4">
         <Navbar />
 
-        <v-main>
-        <router-view class="mx-4 mb-4"/>
-        </v-main>
+        <!-- Transitions for my views -->
+        <transition name="slither" mode="out-in">
+            <router-view class = 'mx-4 mb-4'/>
+        </transition>
+
     </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Navbar from "@/components/Navbar.vue"
+import Navbar from "@/components/Navbar.vue";
 
 // Setting Website Name
 document.title = "ISAD";
@@ -23,3 +25,17 @@ export default Vue.extend({
     }),
 });
 </script>
+
+<style>
+.slither-enter-active, .slither-leave-active {
+  transition: transform 1s;
+}
+
+.slither-enter, .slither-leave-to {
+  transform: translateX(-100%);
+}
+
+.slither-enter-to, .slither-leave {
+  transform: translateX(0);
+}
+</style>
