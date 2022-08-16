510a469f08ba99b5be5bd2dff5a062135fa07a44
Haloen <72333894+Haloen@users.noreply.github.com>
2022-08-16 22:58:53 +0800

    Hooked up a progress bar to router

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,103 @@
 <template>
     <v-app class="grey lighten-4">
-        <Navbar />
+        <Navbar class='hidden-md-and-down'/>
+        <v-step-progress
+          :steps="['CDT', 'MMSE', 'CVDT', 'Results']"
+          passive-color="#b5b5b5"
+          active-color="#23d160"
+          :passive-thickness="5"
+          :active-thickness="5"
+          :line-thickness="5"
+          :current-step="step"
+          icon-class="mdi-check"/>
 
         <!-- Transitions for my views -->
-        <transition name="slither" mode="out-in">
+        <transition :name="'slider-' + anim_dir" mode='out-in'>
             <router-view class = 'mx-4 mb-4'/>
         </transition>
 
     </v-app>
 </template>
 
+
+
 <script lang="ts">
 import Vue from "vue";
 import Navbar from "@/components/Navbar.vue";
+import StepProgress from "vue-step-progress";
+import 'vue-step-progress/dist/main.css';
 
 // Setting Website Name
 document.title = "ISAD";
 
+// Route to Steps
+const steps: string[] = [
+    "Dashboard",
+    "Projects", 
+    "Team", 
+];
+
 export default Vue.extend({
     name: "App",
-    components: {Navbar},
+    components: {Navbar, "v-step-progress": StepProgress},
     data: () => ({
-        //
+        step: 0,
+        anim_dir: "right"
     }),
+
+    watch: { // Observer for route changes
+
+        $route (to, from){ // Changes Progress bar accordingly
+            console.log(to);
+            console.log(from);
+
+            // What is the next index position
+            var next: number = steps.indexOf(to.name);
+
+            // Handles Animation Slide Direction
+            if (next < this.step) this.anim_dir = "left";
+            else this.anim_dir = "right";
+
+            // Finally sets the current index
+            this.step = next;
+        }
+    }
 });
 </script>
 
+
+
+<!-- Slide left & right animations -->
 <style>
-.slither-enter-active, .slither-leave-active {
-  transition: transform 1s;
+.slider-right-enter {
+  opacity: 0;
+  transform: translatex(100%);
 }
 
-.slither-enter, .slither-leave-to {
-  transform: translateX(-100%);
+.slider-right-enter-to {
+  opacity: 1;
+  transition: all 0.5s ease-out;
 }
 
-.slither-enter-to, .slither-leave {
-  transform: translateX(0);
+.slider-right-leave-to {
+  opacity: 0;
+  transform: translatex(-100%);
+  transition: all 0.5s ease-out;
+}
+
+.slider-left-enter {
+  opacity: 0;
+  transform: translatex(-100%);
+}
+
+.slider-left-enter-to {
+  opacity: 1;
+  transition: all 0.5s ease-out;
+}
+
+.slider-left-leave-to {
+  opacity: 0;
+  transform: translatex(100%);
+  transition: all 0.5s ease-out;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
         <v-toolbar flat app>
             <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-            <v-toolbar-title class = 'text-uppercase grey--text'>
+            <v-toolbar-title class = "text-uppercase grey--text">
                 <div>ISAD</div>
             </v-toolbar-title>
 
@@ -16,7 +16,7 @@
 
         </v-toolbar>
 
-        <v-navigation-drawer v-model='drawer' temporary app color=primary>
+        <v-navigation-drawer v-model="drawer" temporary app color=primary>
             <v-list>
                 <!-- Lists all the items in the nav bar -->
                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
@@ -44,9 +44,9 @@ export default Vue.extend({
     data: () => ({
         drawer: false,
         links: [
-            { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
-            { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
-            { icon: 'mdi-account', text: 'Team', route: '/team' },
+            { icon: "mdi-view-dashboard", text: "Dashboard", route: "/" },
+            { icon: "mdi-folder", text: "My Projects", route: "/projects" },
+            { icon: "mdi-account", text: "Team", route: "/team" },
         ]
     })
 });
